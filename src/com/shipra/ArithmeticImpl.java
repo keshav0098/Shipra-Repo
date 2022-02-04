03de49db7f60b32d216906b5e4bd29bcfb27f02f
Keshav <aggarwalk721@gmail.com>
2022-02-04 18:54:07 +0530

    my first commit

---
+++ src/com/shipra/ArithmeticImpl.java
@@ -0,0 +1,18 @@
+package com.shipra;
+
+public class ArithmeticImpl {
+public static void main(String[] args) {
+	add();
+	sub();
+}
+
+private static void sub() {
+	System.out.println("Addition");
+	
+}
+
+private static void add() {
+	System.out.println("Substraction");
+	
+}
+}
